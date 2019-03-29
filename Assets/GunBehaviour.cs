e7c89423db21f48e2aebbe56ad116b6199755af3
Jfoy <39011272+JamesFoy@users.noreply.github.com>
2019-03-29 14:55:43 +0000

    Added Prototype Objects

---
+++ Assets/GunBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    GameObject bullet;
+
+    [SerializeField]
+    float bulletForce;
+
+    [SerializeField]
+    Transform firePoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Shoot()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        Rigidbody body = spawnedBullet.GetComponent<Rigidbody>();
+        body.AddForce(transform.right * bulletForce, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/Target/TargetBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            anim.SetTrigger("beenShot");
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	}
+}
